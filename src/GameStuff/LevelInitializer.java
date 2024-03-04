420e124b20720a020b995017912340534235c024
huynh-tristan <huynh.t.tristan@gmail.com>
2024-03-04 07:41:32 -0500

    In preparation of final demo, broke down the original first level into multiple levels to slowly introduce the features we worked on

--- src/GameStuff/LevelInitializer.java
+++ src/GameStuff/LevelInitializer.java
@@ -1,8 +1,6 @@
 package GameStuff;
 
-import Levels.CompletionState;
-import Levels.GameOverState;
-import Levels.LevelOne;
+import Levels.*;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -14,7 +12,17 @@ public class LevelInitializer extends StateBasedGame {
 
     public void initStatesList(GameContainer gameContainer) {
         //Add start screen
+
+        //Level 1 shows basic movement, simple gravity with the player, and player hitbox
         this.addState(new LevelOne());
+        //Level 2 shows bouncing of a circular entity and collision between the player and the ball
+        this.addState(new LevelTwo());
+        //Level 3 shows off a sample enemy agent
+        this.addState(new LevelThree());
+        //Level 4 could show off the ball interaction with a rotated box
+
+        //Level 5 could show off the player with a rotated box
+
         this.addState(new GameOverState());
         this.addState(new CompletionState());
     }
--- src/Levels/LevelThree.java
+++ src/Levels/LevelThree.java
@@ -1,36 +1,54 @@
 package Levels;
 
 import GameStuff.Agent;
-import GameStuff.Player;
 import GameStuff.Ball;
+import GameStuff.Player;
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
+
 import java.io.File;
 
-
-public class LevelOne extends BasicGameState {
-
+public class LevelThree extends BasicGameState {
     private Player player;
+    private Ball ball;
+    private Player secondEntity;
     private Level lvlOne;
+    private Agent testAgent;
 
     public int getID() {
-        return 0;
+        return 4;
     }
 
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) {
         defineLevel();
         Image playerImage;
-
+        Image ballImage;
+        Image secondEntityImage;
+        Image testAgentImage;
+        File xmlFile;
         try {
             playerImage = new Image("./src/res/Platformer_Art_Complete_Pack/Base pack/Player/p1_front.png");
+            ballImage = new Image("./src/res/Platformer_Art_Complete_Pack/Base pack/Items/bomb.png");
+            //secondEntityImage = new Image("./src/res/Platformer_Art_Complete_Pack/Base pack/Player/p1_front.png");
+            secondEntityImage = new Image("./src/res/Platformer_Art_Complete_Pack/Base pack/Enemies/fishSwim1.png");
+            testAgentImage = new Image("./src/res/Platformer_Art_Complete_Pack/Base pack/Enemies/blockerMad.png");
+            xmlFile = new File("./src/res/sampleTree.xml");
         } catch (SlickException e) {
             throw new RuntimeException(e);
         }
         player = new Player(playerImage, 3,368,0.5f, lvlOne.getLvlData());
+        ball = new Ball(ballImage, 432, 68, 0.5f,  lvlOne.getLvlData());
+        secondEntity = new Player(secondEntityImage, 200, 100, 0.5f, lvlOne.getLvlData());
+        secondEntity.setRotation(160);
+
+        testAgent = new Agent(testAgentImage, 100, 100, 1, lvlOne.getLvlData(), xmlFile);
+
+
+        ball.appendToEntityList(player);
         gameContainer.getInput().addKeyListener(new KeyboardInputs(this.player));
         gameContainer.getInput().addMouseListener(new MouseInputs(this.player));
     }
@@ -64,20 +82,45 @@ public class LevelOne extends BasicGameState {
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         TiledMap tm = new TiledMap("src/res/438Map.tmx", "src/res");
         tm.render(0,0);
-        graphics.drawString("Level 1", 400, 400);
+        graphics.drawString("Level 3", 400, 400);
         player.renderFront();
+        ball.renderFront();
+        secondEntity.renderFront();
         player.drawHitbox(graphics);
+        ball.drawHitbox(graphics);
+        secondEntity.drawHitbox(graphics);
+
+        testAgent.renderFront();
     }
 
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) {
         player.update();
+        ball.update();
+        testAgent.update();
+        secondEntity.update();
         if(player.getXDelta() > 910) {
             player.updateLocation(3,368);
-            stateBasedGame.enterState(3);
+            stateBasedGame.enterState(2);
+        }
+        if(ball.getXDelta() > 910) {
+            ball.setRight(false);
+        }
+        if(ball.getXDelta() < 40) {
+            ball.setLeft(false);
         }
         if(gameContainer.getInput().isKeyPressed(Input.KEY_0)){
             player.updateLocation(3,368);
             stateBasedGame.enterState(1);
+        }
+        if(gameContainer.getInput().isKeyPressed(Input.KEY_1)){
+            ball.setRight(false);
+            ball.setLeft(false);
+            ball.setYDelta(100);
+        }
+        if(gameContainer.getInput().isKeyPressed(Input.KEY_2)){
+            ball.setRight(false);
+            ball.setLeft(false);
+            ball.setXDelta(432);
         }
     }
 }
